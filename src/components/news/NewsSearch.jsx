87aad635da2c103c66ff1d0100ba3797f6bee73a
Franco Ortega <francopdx@gmail.com>
2021-02-03 15:46:22 -0800

    add functionality to search bar

--- src/components/news/NewsSearch.jsx
+++ src/components/news/NewsSearch.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { getNewsArticles } from '../../services/newsApi';
 
 export default class NewsSearch extends Component {
+  state = {
+    searchTerm: 'news',
+    newsArticles: []
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ searchTerm: target.value });
+  }
+
+  handleSearchTermSubmit = (event) => {
+    event.preventDefault();
+    const { searchTerm } = this.state;
+    getNewsArticles(searchTerm)
+      .then(newsArticles => this.setState({ newsArticles }));
+  }
+
   render() {
     return (
       <>
         Search for news articles:
-        <input />          
+        <form onSubmit={this.handleSearchTermSubmit}>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="search the news"
+            value={this.state.searchTerm} />
+          <button>Search</button>
+        </form>
       </>
     );
   }
--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,6 +1,7 @@
-export const getNewsArticles = () => {
+export const getNewsArticles = async(searchTerm) => {
+
   // eslint-disable-next-line max-len
-  return fetch(`https://newsapi.org/v2/everything?q=${searchIput}&apiKey=${process.env.API_KEY}`)
+  return await fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${process.env.API_KEY}`)
     .then(res => res.json())
-    .then(json => json.artciles);
+    .then(json => json.articles);
 };

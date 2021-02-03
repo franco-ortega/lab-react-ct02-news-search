d6e434d7109bc72673c1572ddb6bc952ffdc5b1d
Franco Ortega <francopdx@gmail.com>
2021-02-03 15:51:42 -0800

    add fdata to display in NewsArtcle.jsx

--- src/components/news/NewsArticle.jsx
+++ src/components/news/NewsArticle.jsx
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class NewsArticle extends Component {
-  render() {
-    return (
-      <>
-        This is a news article.
-      </>
-    );
-  }
-}
+const NewsArticle = ({ author, title, description }) => (
+  <>
+    <h3>{title}</h3>
+    <p>By {author}</p>
+    <p>{description}</p>
+  </>
+
+);
+
+NewsArticle.propTypes = {
+  author: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default NewsArticle;
+
+// export default class NewsArticle extends Component {
+//   render() {
+//     return (
+//       <>
+//         This is a news article.
+//       </>
+//     );
+//   }
+// }
